6d931e3326cc48fe6560f20cfce8f4be290522a6
Maya Chisem <chisem.maya@gmail.com>
2020-03-01 19:29:48 -0800

    update

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
 const fs = require("fs");
 const axios = require("axios");
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+
+    axios.get(queryUrl)
+    .then(function (response) {
+      //map will always return an array//
+      const repoNames = response.data.map(function(repo){
+        return repo.name;
+      });
+      console.log(response);
+
+      //slash n means new line//
+      const repoNamesStr = repoNames.join("\n");
+
+      fs.writeFile("repos.txt", repoNamesStr, function(err){
+        if(err) {
+          throw err;
+        }
+      });
+    })
+  
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .finally(function () {
+      // always executed
+    });
+  
+  });

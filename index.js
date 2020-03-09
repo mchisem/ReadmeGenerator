a8aa2a2f6d5c7e71ee1fed4e63008f197ca3d39d
Maya Chisem <chisem.maya@gmail.com>
2020-03-09 00:26:50 -0700

    update

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-// const util = require("util");
 const axios = require("axios");
 
-// const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer
 .prompt([
